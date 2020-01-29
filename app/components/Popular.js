742d881b041774dc52514322921054f180e8db6c
Luke Seo <lukeyseo@gmail.com>
2020-01-28 17:55:05 -0800

    Added more info to each grid

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { fetchPopularRepos } from '../utils/api'
+import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
     const languages = ['All', 'JavaScript', 'Java', 'Ruby', 'Python']
@@ -31,7 +32,7 @@ function ReposGrid({ repos }) {
     return (
         <ul className='grid space-around'>
             {repos.map((repo, index) => {
-                const { name, owner, html_url, stargazers_count, fork, open_issues } = repo;
+                const { name, owner, html_url, stargazers_count, forks, open_issues } = repo;
                 const { login, avatar_url } = owner;
 
                 return (
@@ -47,6 +48,30 @@ function ReposGrid({ repos }) {
                         <h2 className='centerText'>
                             <a className='link' href={ html_url }>{ login }</a>
                         </h2>
+
+                        <ul className='card-list'>
+                            <li>
+                                <FaUser color='rgb(255, 191, 116)' size={22}/>
+                                <a href={`https://github.com/${login}`}>
+                                    {login}
+                                </a>
+                            </li>
+
+                            <li>
+                                <FaStar color='rgb(255, 215, 0)' size={22}/>
+                                {stargazers_count.toLocaleString()} stars
+                            </li>
+
+                            <li>
+                                <FaCodeBranch color='rgb(129, 195, 245)' size={22}/>
+                                {forks.toLocaleString()} forks
+                            </li>
+
+                            <li>
+                                <FaExclamationTriangle color='rgb(241, 138, 147)' size={22}/>
+                                {open_issues.toLocaleString()} open issues
+                            </li>
+                        </ul>
                     </li>
                 )
             })}
